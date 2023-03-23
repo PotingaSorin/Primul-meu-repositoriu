841c30bd3d95fad39d0e3614d78324ee8f071eb6
PotingaSorin <126877303+PotingaSorin@users.noreply.github.com>
2023-03-23 09:54:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+let form = document.querySelecter('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  return false;
+});
